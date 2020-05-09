4070b3b95856c93b5fd4699d450941b9d1ee7919
Maria Diaz <adiaz064@gmail.com>
2020-05-09 13:56:05 -0700

    Added cart session instantiation

--- loginVerify.php
+++ loginVerify.php
@@ -2,16 +2,18 @@
 // include files
 require_once('query.php');
 require_once('validation.php');
-
-if(isset($_POST["submit"])){ // when submit is pressed
+$_SESSION['cart'] = array();
+if(isset($_POST["submit"])){ // when subit is pressed
 // assign variables   
-$username = validUsername($_POST["username"]);
-$password = validPassword($_POST["password"]);
+$username = validUser($_POST["username"],"username");
+$password = validUser($_POST["password"], "password");
 
 login($username, $password);
+
 // if login: start session 
 session_start();
 $_SESSION['valid_user'] = $username;
+// move to home.php
 header( 'Location: home.php');
 }
 ?>
